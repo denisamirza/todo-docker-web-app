afbfab73b83f95e04062dfa6c6042136e463b789
Denisa Mirza <denisamirza1@gmail.com>
2024-02-28 10:18:26 +0200

    Make localhost deployment public

--- aws-deployment/main.tf
+++ aws-deployment/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-denisa-bucket"
+    bucket = "terraform-state-deni-bucket"
     key    = "terraform-tfstate"
     region = "us-east-1"
   }
